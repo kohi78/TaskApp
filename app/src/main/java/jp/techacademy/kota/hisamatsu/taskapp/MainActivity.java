d6f5f27805995f5dfa03fa910505ae4f037c5c39
Kota H <kohi78@users.noreply.github.com>
2016-07-20 19:15:27 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kota/hisamatsu/taskapp/MainActivity.java
@@ -0,0 +1,172 @@
+package jp.techacademy.kota.hisamatsu.taskapp;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_TASK = "jp.techacademy.kota.hisamatsu.taskapp.TASK";
+
+    private Realm mRealm;
+    private RealmResults<Task> mTaskRealmResults;
+    private RealmChangeListener mRealmListener = new RealmChangeListener() {
+        @Override
+        public void onChange() {
+            reloadListView();
+        }
+    };
+
+    private EditText mSearchText;
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Realm
+        mRealm = Realm.getDefaultInstance();
+        mTaskRealmResults = mRealm.where(Task.class).findAll();
+        mTaskRealmResults.sort("date", Sort.DESCENDING);
+        mRealm.addChangeListener(mRealmListener);
+
+        // ListView
+        mTaskAdapter = new TaskAdapter(MainActivity.this);
+        mListView = (ListView) findViewById(R.id.listView1);
+
+        mSearchText = (EditText) findViewById(R.id.search_edit_text);
+        Button searchButton = (Button) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mTaskRealmResults = mRealm.where(Task.class).contains("category", mSearchText.getText().toString()).findAll();
+                mTaskRealmResults.sort("date", Sort.DESCENDING);
+                mRealm.addChangeListener(mRealmListener);
+
+                reloadListView();
+            }
+        });
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Task task = (Task) parent.getAdapter().getItem(position);
+
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                intent.putExtra(EXTRA_TASK, task);
+
+                startActivity(intent);
+            }
+        });
+
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                final Task task = (Task) parent.getAdapter().getItem(position);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("削除");
+                builder.setMessage(task.getTitle() + "を削除しますか？");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RealmResults<Task> results = mRealm.where(Task.class).equalTo("id", task.getId()).findAll();
+
+                        mRealm.beginTransaction();
+                        results.clear();
+                        mRealm.commitTransaction();
+
+                        Intent resultIntent = new Intent(getApplicationContext(), TaskAlarmReceiver.class);
+                        PendingIntent resultPendingIntent = PendingIntent.getBroadcast(
+                                MainActivity.this,
+                                task.getId(),
+                                resultIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+
+                        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                        alarmManager.cancel(resultPendingIntent);
+
+                        reloadListView();
+                    }
+                });
+                builder.setNegativeButton("CANCEL", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                return true;
+            }
+        });
+
+        reloadListView();
+    }
+
+    private void reloadListView() {
+
+        ArrayList<Task> taskArrayList = new ArrayList<>();
+
+        for (int i = 0; i < mTaskRealmResults.size(); i++) {
+            Task task = new Task();
+
+            task.setId(mTaskRealmResults.get(i).getId());
+            task.setTitle(mTaskRealmResults.get(i).getTitle());
+            task.setContents(mTaskRealmResults.get(i).getContents());
+            task.setDate(mTaskRealmResults.get(i).getDate());
+            task.setCategory(mTaskRealmResults.get(i).getCategory());
+
+            taskArrayList.add(task);
+        }
+
+        mTaskAdapter.setTaskArrayList(taskArrayList);
+        mListView.setAdapter(mTaskAdapter);
+        mTaskAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mRealm.close();
+    }
+
+}
---
+++ app/src/main/java/jp/techacademy/kota/hisamatsu/taskapp/Task.java
@@ -0,0 +1,57 @@
+package jp.techacademy.kota.hisamatsu.taskapp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Task extends RealmObject implements Serializable {
+    private String title;
+    private String contents;
+    private Date date;
+    private String category;
+
+    @PrimaryKey
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
